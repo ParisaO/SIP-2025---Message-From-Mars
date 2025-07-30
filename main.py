93676f9448d0cc9d169a05c9e63f414c250e3f99
stephensheng2 <stephensheng09@gmail.com>
2025-07-30 13:43:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from Source_Coding import one_hot_encoder, one_hot_decoder, parity_encoder, parity_decoder    # Add Hamming
+from Source_Coding import one_hot_encoder, one_hot_decoder, parity_encoder, parity_decoder, hamming_encoder, hamming_decoder    # Add Hamming
 from Channel_Modulation import bpsk_modulate, bpsk_demodulate, qpsk_modulate, qpsk_demodulate
 from message_to_index import message_to_index    
 from AWGN import add_awgn_noise
